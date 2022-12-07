2495ae6c3a61d3cabf74fd5f6c9a1dbb7d75e14f
yclark4 <84817430+yclark4@users.noreply.github.com>
2022-12-06 21:58:47 -0500

    Initial commit

---
+++ app/src/main/java/com/zybooks/dolphinlive/UI/MainActivity.java
@@ -0,0 +1,29 @@
+package com.zybooks.dolphinlive.UI;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.zybooks.dolphinlive.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Button button = findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view){
+
+                Intent intent = new Intent(MainActivity.this, ProductList.class);
+                startActivity(intent);
+            }
+        });
+
+    }
+}
